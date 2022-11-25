6156830e7759d799357540baf518f0cc14bf0439
kj-dev <agassi77.dev@gmail.com>
2022-11-25 15:40:03 +0900

    Add components and handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./assets/App.css";
+import Initial from "./components/initial";
+import Confirm from "./components/confirm";
+import Result from "./components/result";
+
+const handleComponent = () => {
+  const screen = 0;
+
+  if (screen === 0) return <Initial />;
+  if (screen === 1) return <Confirm />;
+  if (screen === 2) return <Result />;
+};
 
 const App = () => {
-  return <div className="container">Hello</div>;
+  return <div className="container">{handleComponent()}</div>;
 };
 
 export default App;
